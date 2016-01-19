d5f0796d0c4d10d06be83419601d46642556a5a1
Ilya Sname <Ilya_hr2@mail.ru>
2016-01-19 13:24:52 +0300

    fourth lesson

--- lab.js
+++ lab.js
@@ -1,3 +1,45 @@
-﻿/*
+﻿function sayHello(){
+	print("Hello, world!");
+}
+
+sayHello();
+
+
+
+/*
+	var num = readline();
+	var str = "На дереве сидит " + num + " ворон";
+	var x = "";
+
+	if(num%100 < 11 || num%100 > 14){
+		switch(num%10){
+		case 1: x = "а"; break;
+		case 2:
+		case 3:
+		case 4: x = "ы"
+		}
+	}
+	console.log(str + x);
+
+
+
+putstr("Enter number: ");
+var num = readline();
+putstr("Enter exp: ");
+var exp = readline();
+var result = 1;
+var cnt = 1;
+
+while(cnt <= exp){
+	result *= num;
+	cnt++;
+}
+print(result);
+
+
+
 ** Просто пусто файл	 
 */
+
+//var name = 'John';
+//print("Hello, " + name); //не ecmascript 
