df7567947b3097dc163637f283b3bac2eb3d9885
Ilya Snake <Ilya_hr2@mail.ru>
2016-02-05 00:14:50 +0300

    add extended description of closure

--- myExamples/closure.js
+++ myExamples/closure.js
@@ -5,4 +5,17 @@ function say(name){
 }
 
 var x = say('Ilya');
-print(x('Snake')); //it will print --> Ilya Snake
+print(x('Snake')); //We'll get --> Ilya Snake
+
+
+//With comments
+function say(name){
+	//This is anonymous function - it no needs a name. it returns as result of "say" function executing and throw to "x" variable.
+	return function(lastName){
+		return name + " " + lastName;
+	}
+}
+
+var x = say('Ilya'); //this variable get the result of "say" function executing with added parameter. it get anonymous function.
+//Now "x" variable contents DECLARATION of anonymous function. And we call this function with new parameter.
+print(x('Snake')); //We get "Ilya Snake" 
